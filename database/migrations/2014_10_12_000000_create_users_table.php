166a956d4462b1d8eca7584d2ffe1436a5889e10
felipesms2 <felipesms@live.com>
2023-01-26 12:52:01 -0300

    Some addictions on user table

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -16,14 +16,15 @@ return new class extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->id();
                 $table->string('name');
-                $table->longText('bio');
+                $table->longText('bio')->nullable();
                 $table->boolean('can_dm_me')->default(true);
                 $table->boolean('can_public_msg_me')->default(true);
                 $table->string('email')->unique();
-                $table->integer('media_id');
+                $table->integer('media_id_profile_picture')->nullable();
                 $table->integer('language_id')->nullable()->default(null);
                 $table->timestamp('email_verified_at')->nullable();
                 $table->string('password');
+                $table->integer("score_sum")->default(0); // Sum score from reputation
                 $table->rememberToken();
             $table->timestamps();
         });
