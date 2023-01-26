78275df9374b4f8d63d24fc0907be660aded7e61
felipesms2 <felipesms@live.com>
2023-01-26 13:19:02 -0300

    Testing user factory with new fields

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -17,9 +17,14 @@ class UserFactory extends Factory
      */
     public function definition()
     {
+        $canDM = $this->faker->randomElement([0,1]);
+        $canPublic = $this->faker->randomElement([0,1]);
+
         return [
             'name' => fake()->name(),
             'email' => fake()->unique()->safeEmail(),
+            'can_dm_me' => $canDM,
+            'bio' => fake()->text(max([500])),
             'email_verified_at' => now(),
             'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
             'remember_token' => Str::random(10),
