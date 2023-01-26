766a8addb0047d887e47bd8a67717b4882b96b48
felipesms2 <felipesms@live.com>
2023-01-26 13:00:34 -0300

    Reputation and user relationship in model

--- app/Models/reputation.php
+++ app/Models/reputation.php
@@ -4,8 +4,13 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use App\Models\User;
 
 class reputation extends Model
 {
     use HasFactory;
+    public function user()
+        {
+            return $this->belongsTo(User::class);
+        }
 }
